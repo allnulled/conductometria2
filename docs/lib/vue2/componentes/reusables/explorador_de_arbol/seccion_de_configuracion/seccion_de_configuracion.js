0a11a411dda431d5b328fbada524bd1a444f3bff
allnulled <todosanulados@gmail.com>
2024-10-01 01:25:57 +0200

    interfaz pacifica

--- docs/lib/vue2/componentes/reusables/explorador_de_arbol/seccion_de_configuracion/seccion_de_configuracion.js
+++ docs/lib/vue2/componentes/reusables/explorador_de_arbol/seccion_de_configuracion/seccion_de_configuracion.js
@@ -15,13 +15,15 @@ return await Sistema_de_modulos.definir_componente_vue2(
     data() {
       this.$utilidades.tracear("seccion_de_configuracion.data");
       return {
-        metadatos_de_reporte: this.explorador.datos_de_reportes_relativos
+        panel_seleccionado: "", // "importar", "exportar", ""
+        metadatos_de_reporte: this.explorador.datos_de_reportes_relativos,
+        texto_de_importar: "",
+        texto_de_exportar: "",
       }
     },
     methods: {
       guardar_metadatos_de_reportes_relativos() {
         this.$utilidades.tracear("seccion_de_configuracion.guardar_metadatos_de_reportes_relativos");
-        console.log(this.$refs);
         const nombre = this.$refs.nombre_de_reporte.value;
         const modificador = this.$refs.modificador_de_reporte.value;
         const plantilla = this.$refs.plantilla_de_reporte.value;
@@ -29,6 +31,33 @@ return await Sistema_de_modulos.definir_componente_vue2(
         return this.explorador.guardar_metadatos_de_reportes_relativos(nombre, modificador, plantilla, componente).then(() => {
           this.explorador.ir_a_seccion("configuracion");
         });
+      },
+      limpiar_reportes() {
+        this.panel_seleccionado = "";
+        delete localStorage[this.explorador.explorador_raiz.identificador_de_reportes];
+        return this.explorador.explorador_raiz.cargar_memoria_de_reportes_absolutos(true);
+      },
+      alternar_panel_de_importar_reportes() {
+        if(this.panel_seleccionado === "importar_reportes") {
+          this.panel_seleccionado = "";
+        } else {
+          this.panel_seleccionado = "importar_reportes";
+        }
+      },
+      alternar_panel_de_exportar_reportes() {
+        if(this.panel_seleccionado === "exportar_reportes") {
+          this.panel_seleccionado = "";
+        } else {
+          const reportes = JSON.stringify(this.explorador.explorador_raiz.datos_de_reportes_absolutos, null, 2);
+          this.texto_de_exportar = reportes;
+          this.panel_seleccionado = "exportar_reportes";
+        }
+      },
+      importar_reportes() {
+
+      },
+      copiar_texto_de_exportar_reportes() {
+        
       }
     },
     mounted() {
